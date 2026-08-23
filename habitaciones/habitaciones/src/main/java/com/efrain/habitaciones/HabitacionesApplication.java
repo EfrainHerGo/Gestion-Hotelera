2bd782ac5cdca4f0974370dda8bad7f244192b23
EfrainHer <za181945@gmail.com>
2026-08-23 10:16:30 -0600

    eureka y gateway ya estan cofiguradas

--- habitaciones/habitaciones/src/main/java/com/efrain/habitaciones/HabitacionesApplication.java
+++ habitaciones/habitaciones/src/main/java/com/efrain/habitaciones/HabitacionesApplication.java
@@ -2,8 +2,10 @@ package com.efrain.habitaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
+@SpringBootApplication(scanBasePackages = {"com.efrain.habitaciones", "com.efrain.Common"})
 public class HabitacionesApplication {
 
 	public static void main(String[] args) {
--- huesped/huesped/src/main/java/com/efrain/huesped/HuespedApplication.java
+++ huesped/huesped/src/main/java/com/efrain/huesped/HuespedApplication.java
@@ -2,8 +2,10 @@ package com.efrain.huesped;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HuespedApplication {
 
 	public static void main(String[] args) {
