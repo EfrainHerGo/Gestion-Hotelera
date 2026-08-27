ca719ee335ea1c36ffefd437a39ade596a115f1f
varelahuelitlvictormanuel-netizen <https://github.com/varelahuelitlvictormanuel-netizen>
2026-08-27 11:55:27 -0600

    Validaciones y integracion del id

--- habitaciones/habitaciones/src/main/java/com/efrain/habitaciones/controller/HabitacionController.java
+++ habitaciones/habitaciones/src/main/java/com/efrain/habitaciones/controller/HabitacionController.java
@@ -13,8 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Validated
-public class HabitacionController
-        extends CommonController<HabitacionRequest, HabitacionResponse, HabitacionService> {
+public class HabitacionController extends CommonController<HabitacionRequest, HabitacionResponse, HabitacionService> {
 
     public HabitacionController(HabitacionService service) {
         super(service);
